44eeeeb8f6b31df60f30741566601f2c381bc159
Guilherme Pessoa <guipessoagith@gmail.com>
2023-09-19 02:18:33 -0300

    update commit

--- Backend_API/backend_eduplan/README.md
+++ Backend_API/backend_eduplan/README.md
@@ -1,34 +1,27 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+## Projeto
 
-## Getting Started
+ Autenticação Social (Github) no NextJS utilizando NextAuth.
 
-First, run the development server:
+![Untitled-1](https://github.com/GuiCPessoa/Social_Login/assets/93964438/81830494-2802-428d-ad62-91372916f992)
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
+
+## Como rodar
+
+Para iniciar o projeto, primeiramente instale as dependências utilizando:
+
+```
+yarn
 ```
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+Após, rode:
 
-You can start editing the page by modifying `app/page.js`. The page auto-updates as you edit the file.
+```
+yarn dev
+```
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
+Para iniciar o Projeto
 
-## Learn More
 
-To learn more about Next.js, take a look at the following resources:
+## Erros
+No meu caso deu alguns erros na instalação do Yarn, assim como qualquer coisa que seja tipo o npm. Apenas copiei as linhas de erro e joguei no ChatGPT, segui os passos e consegui resolver.
 
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
---
+++ Backend_API/backend_eduplan/src/pages/login/index.tsx
@@ -0,0 +1,117 @@
+import { Box, Button, Flex, Input, InputGroup, InputLeftElement, Link, Text, useTheme } from "@chakra-ui/react";
+import { FiGithub, FiLock, FiUser } from 'react-icons/fi';
+import { signIn } from 'next-auth/react';
+import { getSession } from 'next-auth/react';
+import { GetServerSideProps } from 'next';
+import { FormEvent } from "react";
+
+
+
+
+function Login() {
+  const theme = useTheme();
+
+  const onSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    signIn('credentials');
+  }
+
+
+  return (
+    <Flex
+      width="100%"
+      height="100vh"
+      alignItems="center"
+      justifyContent="center">
+      <form onSubmit={onSubmit}>
+        <Box
+          width="428px"
+        >
+          <Text
+            color="gray.100"
+            fontSize="xl"
+            textAlign="center"
+            fontFamily="heading"
+            fontWeight={600}
+            marginBottom="2rem">Sign in to your account</Text>
+          <InputGroup>
+            <InputLeftElement
+              pointerEvents='none'
+            >
+              <FiUser color={theme.colors.gray['500']} />
+            </InputLeftElement>
+            <Input
+              placeholder="Email"
+              marginBottom="1rem"
+              borderColor="gray.700"
+              backgroundColor="gray.700"
+              color="gray.100" />
+          </InputGroup>
+          <InputGroup>
+            <InputLeftElement
+              pointerEvents='none'
+            >
+              <FiLock color={theme.colors.gray['500']} />
+            </InputLeftElement>
+            <Input
+              placeholder="Senha"
+              borderColor="gray.700"
+              backgroundColor="gray.700"
+              color="gray.100"
+              type="password" />
+          </InputGroup>
+
+          <Flex direction="column" alignItems="center">
+            <Button
+              type="submit"
+              marginTop="2rem"
+              marginBottom="1rem"
+              width="100%"
+              backgroundColor="secondary.500"
+              _hover={{ backgroundColor: "secondary.600" }}>Entrar</Button>
+            <Link
+              href="#"
+              color="gray.100">
+              Não tem uma conta?<Text as="span" color="secondary.500"> Registre-se</Text>
+            </Link>
+          </Flex>
+
+          <Flex marginTop="2rem" alignItems="center" justifyContent="center">
+            <Text
+              color="secondary.500"
+              textAlign="center"
+              marginRight="1rem">Ou entre com</Text>
+            <Button
+              backgroundColor="gray.600"
+              leftIcon={<FiGithub color={theme.colors.gray['100']} />}
+              color={theme.colors.gray['100']}
+              _hover={{ backgroundColor: "gray.700" }}
+              onClick={() => signIn('github')}>Github</Button>
+          </Flex>
+        </Box>
+      </form>
+    </Flex>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getSession(context);
+
+  if(session){
+    return {
+      redirect: {
+        destination: '/home',
+        permanent: false
+
+      }
+    }
+  }
+
+  return {
+    props: {
+      session
+    }
+  }
+}
+
+export default Login;
